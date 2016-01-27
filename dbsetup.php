ca5af225ab9b59ca97304b1cdbda79f45327bb46
Lisa Thompson <redcypress@yahoo.com>
2016-01-27 22:31:48 +0000

    asdf

--- dbsetup.php
+++ dbsetup.php
@@ -7,14 +7,24 @@ $dbname = "artacre";
 $usertable = "products";
 $yourfield = "name";
 
+
+$mysqli = new mysqli($hostname, $username, $password, $dbname);
+
+
 mysqli_connect($hostname, $username, $password) or die ("<html><script language='JavaScript'>
 
 
 alert('Unable to connect to database! Please try again later.'))</script></html>");
 
-mysqli_select_db($dbname);
+
+/* return name of current default database */
+if ($result = $mysqli->query("SELECT DATABASE()")) {
+    $row = $result->fetch_row();
+    printf("Default database is %s.\n", $row[0]);
+    $result->close();
+}
 
 # Check If Record Exists
-$query = "SELECT * FROM $usertable";
-$result = mysqli_query($query);
-if($result){  while($row = mysqli_fetch_array($result))  {    $name = $row["$yourfield"];    echo "Name: ".$name."<br>";  }}?>
+#$query = "SELECT * FROM $usertable";
+#$result = mysqli_query($query);
+#if($result){  while($row = mysqli_fetch_array($result))  {    $name = $row["$yourfield"];    echo "Name: ".$name."<br>";  }}?>
