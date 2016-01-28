3f1b63b9708d2817f01bb0a8959015b0b22d30ba
Lisa Thompson <redcypress@yahoo.com>
2016-01-28 20:27:12 +0000

    images

--- dbsetup.php
+++ dbsetup.php
@@ -70,6 +70,29 @@ $strSQL = "Select * From Products";
 $result = mysqli_query($con, $strSQL); ?>
 
 
+<table id="myTable">
+    <thead>
+        <tr><th>Product name</th><th>Qty</th><th>Price</th>
+    <th align="center"><span id="amount" class="amount">Amount</span> </th></tr>
+    </thead>
+    <tfoot>
+        <tr><td colspan="2"></td><td align="right"><span id="total" class="total">TOTAL</span> </td></tr>
+    </tfoot>
+    <tbody>
+    
+    <tr><td>Product 1</td><td><select value="" class="qty" name="qty">
+        <option value="1">1</option>
+        <option value="2">2</option>
+</select></td><td><input type="text" value="11.60" class="price"></td>
+<td align="center"><span id="amount" class="amount">0</span> eur</td></tr>
+    <tr><td>Product 2</td><td><select value="" class="qty" name="qty">
+<option value="1">1</option>
+<option value="2">2</option>
+</select></td><td><input type="text" value="15.26" class="price"></td>
+<td align="center"><span id="amount" class="amount">0</span> eur</td></tr>
+        
+</tbody></table>
+
 <table width="800" border="0" align="center" cellpadding="0" cellspacing="0" id="producttable">
     <tbody>
     <tr>
