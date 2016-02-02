0469c67b8ba6b365beb216fcab095ca10c29d613
Lisa Thompson <redcypress@yahoo.com>
2016-02-02 03:38:38 +0000

    adding send.php

--- send.php
+++ send.php
@@ -42,7 +42,7 @@ a:link {
 
 
 // Who should this message be sent to?
-$to = "lisa@builtbyredcypress.com, redcypress5246@gmail.com";
+$to = "lisa@builtbyredcypress.com, redcypress5246@gmail.com, lthompson@cellcontrol.com";
 // EXAMPLES
 // $to = "a.n.example@shef.ac.uk"; // one recipient
 // $to = "a.n.example@shef.ac.uk, person.two@shef.ac.uk"; // two recipients
